2e146bff89c899671ac3639a89b6137b5d7f3283
alvaroloal <alorentealman@gmail.com>
2024-10-29 21:01:44 +0100

    act

--- js/script.js
+++ js/script.js
@@ -7,4 +7,12 @@ document.querySelectorAll('.sidebar nav a').forEach(anchor => {
     });
 });
 
+// Alternar modo oscuro
+document.getElementById('toggleDarkMode').addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+    const isDarkMode = document.body.classList.contains('dark-mode');
+    this.innerHTML = isDarkMode ? '<i class="bi bi-sun"></i> Modo Claro' : '<i class="bi bi-moon"></i> Modo Oscuro';
+});
+
+
 
