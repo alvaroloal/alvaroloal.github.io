f72e20ab8ee2fa72cd3e1b9074b1beceb60b2dc4
alvaroloal <alorentealman@gmail.com>
2024-11-02 02:43:17 +0100

    port

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+// Alternar modo oscuro
+document.getElementById('toggleDarkMode').addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+    const isDarkMode = document.body.classList.contains('dark-mode');
+    this.innerHTML = isDarkMode ? '<i class="bi bi-sun"></i> Modo Claro' : '<i class="bi bi-moon"></i> Modo Oscuro';
+});
+
 document.querySelectorAll('.sidebar nav a').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
@@ -7,12 +14,7 @@ document.querySelectorAll('.sidebar nav a').forEach(anchor => {
     });
 });
 
-// Alternar modo oscuro
-document.getElementById('toggleDarkMode').addEventListener('click', function() {
-    document.body.classList.toggle('dark-mode');
-    const isDarkMode = document.body.classList.contains('dark-mode');
-    this.innerHTML = isDarkMode ? '<i class="bi bi-sun"></i> Modo Claro' : '<i class="bi bi-moon"></i> Modo Oscuro';
-});
+
 
 
 
